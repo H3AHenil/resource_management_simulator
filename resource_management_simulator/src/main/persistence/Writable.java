d25e0415a5cb6c95f9f926eeec82775a5b623fff
henilagrawal <hagraw02@student.ubc.ca>
2025-01-31 14:44:20 -0800

    Delete resource_management_simulator directory

--- resource_management_simulator/src/main/persistence/Writable.java
+++
@@ -1,9 +0,0 @@
-package persistence;
-
-import org.json.JSONObject;
-
-// Interface to allow model classes to return JSON objects
-public interface Writable {
-    // EFFECTS: returns implemented object as JSON object
-    JSONObject toJson();
-}
